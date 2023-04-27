4f1d5298616114b6d99b950a359406e910f9eb35
Ninunutsi <khurtsidzenino4@gmail.com>
2023-04-27 14:07:07 +0400

    validation form

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
+
 function App() {
+  const [values, setValues] = useState({
+    firstName: '',
+    lastName: '',
+    email: '',
+    password: ''
+  })
+
+  const [submited, setSubmited] = useState(false)
+  const [valid, setValid] = useState(false)
+
+  const handleFirstNameInputChange = (event) => {
+    setValues({...values, firstName: event.target.value})
+  }
+
+  const handleLastNameInputChange = (event) => {
+    setValues({...values, lastName: event.target.value})
+  }
+
+  const handlEmailChange = (event) => {
+    setValues({...values, email: event.target.value})
+  }
+
+  const handlePasswordChange = (event) => {
+    setValues({...values, password: event.target.value})
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    if(values.firstName && values.lastName && values.email && values.password) {
+      setValid(true)
+    }
+    setSubmited(true)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form className='form' onSubmit={handleSubmit}>
+        <h1 className='heading'>Registration Form</h1>
+        {submited && valid ? <div className='success-msg'>Success! Thank you for registration!</div> : null}
+        <input 
+        type='text' 
+        placeholder='First Name' 
+        className='input' 
+        value={values.firstName}
+        onChange={handleFirstNameInputChange}></input>
+        {submited && !values.firstName ? <span className='error'>Please enter a first name</span> : null}
+
+        <input type='text' placeholder='Last Name' className='input' 
+        value={values.lastName}
+        onChange={handleLastNameInputChange}></input>
+        {submited && !values.lastName ? <span className='error'>Please enter a last name</span> : null}
+
+        <input type='text' placeholder='Email' className='input' value={values.email}
+        onChange={handlEmailChange}></input>
+        {submited && !values.email ? <span className='error'>Please enter an email</span> : null}
+
+        <input type='password' placeholder='Password' className='input'value={values.password}
+        onChange={handlePasswordChange}></input>
+        {submited && !values.password ? <span className='error'>Please enter a password</span> : null}
+
+        <button className='btn'>Register</button>
+      </form>
     </div>
   );
 }
 
 export default App;
+
